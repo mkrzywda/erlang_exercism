b4e5163a6c432d8da5c8d77ead6b8004f055a424
Maciej Krzywda <43749180+mkrzywda@users.noreply.github.com>
2020-03-04 08:48:49 +0100

    Update leap.erl

--- leap/src/leap.erl
+++ leap/src/leap.erl
@@ -1,7 +1,8 @@
 -module(leap).
 -export([leap_year/1]).
 
+-spec leap_year(integer()) -> boolean().
 leap_year(Year) when Year rem 400 =/= 0 -> false;
 leap_year(Year) when Year rem 100 =/= 0 -> true;
 leap_year(Year) when Year rem 400 =/= 0 -> false;
-leap_year(Year) when is_integer(Year) -> true.
+#leap_year(Year) when is_integer(Year) -> true.
